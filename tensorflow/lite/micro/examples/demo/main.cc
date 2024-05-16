0bae58ce98702639e8d4749ecd41f9bfe7035c4f
cjmcv <398342899@qq.com>
2024-05-16 15:20:11 +0000

    Add a demo project.

--- tensorflow/lite/micro/examples/demo/main.cc
+++ tensorflow/lite/micro/examples/demo/main.cc
@@ -1,7 +1,21 @@
 #include <stdio.h>
+#include "py_tflite_inference.hpp"
 
 int main() {
     printf("hello.\n");
+    pai::infer::PyTfliteInference tflite_infer;
+    tflite_infer.Init("tflite_infer", "TfliteInference", "../models/tf_micro_conv_test_model.int8.tflite");
+
+    int8_t *input_data;
+    uint32_t input_size;
+    tflite_infer.GetInputPtr("serving_default_conv2d_input:0", (void **)&input_data, &input_size);
+
+    for (uint32_t i=0; i<input_size/sizeof(uint8_t); i++)
+        input_data[i] = i % 255;
+
+    tflite_infer.Infer();
+
+    tflite_infer.Print("StatefulPartitionedCall:0");
 
     return 0;
 }
--- tensorflow/lite/micro/examples/demo/tflite_infer.py
+++ tensorflow/lite/micro/examples/demo/tflite_infer.py
@@ -6,22 +6,26 @@ class TfliteInference:
         print("hello TfliteInference.")
 
     def load_model(self, model_path):
+        print("TfliteInference->load_model:", model_path)
         self.interpreter = tf.lite.Interpreter(model_path)
         self.input_details = self.interpreter.get_input_details()
         self.output_details = self.interpreter.get_output_details()
+
         self.interpreter.allocate_tensors()
         self.inputs = {}
         self.outputs = {}
-        for i in range(len(self.input_details)):
-            print("in<", i, ">: ",self.input_details[i]['name'])
-        for i in range(len(self.output_details)):
-            print("out<", i, ">: ",self.output_details[i]['name'])
-    
+
+        # print("\n")
+        # for i in range(len(self.input_details)):
+        #     print("in<", i, ">: ",self.input_details[i])
+        # for i in range(len(self.output_details)):
+        #     print("out<", i, ">: ",self.output_details[i])
+        # print("\n")
+
     def get_io_num(self):
         return len(self.input_details), len(self.output_details)
     
     def get_inputs_detail(self, index):
-        print(self.input_details[index])
         if(str(self.input_details[index]['dtype']) == "class 'numpy.float32>"):
             type_size = 4
         else:
@@ -29,7 +33,6 @@ class TfliteInference:
         return type_size, self.input_details[index]['name'], self.input_details[index]['shape']
     
     def get_outputs_detail(self, index):
-        print(self.output_details[index])
         if(str(self.output_details[index]['dtype']) == "class 'numpy.float32>"):
             type_size = 4
         else:
@@ -50,11 +53,12 @@ class TfliteInference:
 
         for i in range(len(self.output_details)):
             self.outputs[i] = self.interpreter.get_tensor(self.output_details[i]['index'])
+
     ################
 
     def fill_random_inputs(self):
         for i in range(len(self.input_details)):
-            if(str(self.input_details[i]['dtype']) == "class 'numpy.float32>"):
+            if(str(self.input_details[i]['dtype']) == "<class 'numpy.float32'>"):
                 data = np.random.rand(*self.input_details[i]['shape'])
                 data = data.astype(np.float32)
             else:
@@ -64,18 +68,34 @@ class TfliteInference:
 
         return self.inputs
     
-    ################
-
     def get_npy_data(self, npy_data_file):
         data = np.load(npy_data_file)
         print(data.shape)
         print(data)
         return data
     
+    ################
+
+    def export_op(self):
+        tensor_details  = self.interpreter.get_tensor_details()
+        print(dir(self.interpreter))
+        
+        for tensor in tensor_details:
+            print(tensor)
+            i = tensor['index']
+            tensor_name = tensor['name']
+            scales = tensor['quantization_parameters']['scales']
+            zero_points = tensor['quantization_parameters']['zero_points']
+            tensor = self.interpreter.tensor(i)()
+
+            print(i, tensor_name, scales.shape, zero_points.shape, tensor.shape)
+    
 if __name__ == "__main__":
     infer = TfliteInference()
-
+    
     infer.load_model("../models/tf_micro_conv_test_model.int8.tflite")
+    # infer.export_op()
+
     input = infer.fill_random_inputs()
     infer.run()
     output = infer.get_output(0)
